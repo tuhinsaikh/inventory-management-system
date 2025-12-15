2eaa9334099d2b8205cfccdc60a8c950ad259c24
Tuhin Saikh <tsaikh@entiovi.com>
2025-12-15 14:47:36 +0530

    Initial commit

---
+++ src/main/java/com/retailshop/RetailshopApplication.java
@@ -0,0 +1,13 @@
+package com.retailshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailshop/RetailshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
