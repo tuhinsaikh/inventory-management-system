23bfb966c1ba777352b3efc00bb08feb37aa0ef0
Tuhin Saikh <tsaikh@entiovi.com>
2025-12-15 15:04:45 +0530

    entity class added

--- src/main/java/com/retailshop/InventoryManagementApplication.java
+++ src/main/java/com/retailshop/InventoryManagementApplication.java
@@ -2,12 +2,12 @@ package com.retailshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class RetailshopApplication {
-
+@EnableJpaAuditing
+public class InventoryManagementApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RetailshopApplication.class, args);
+		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/retailshop/entity/Supplier.java
@@ -0,0 +1,4 @@
+package com.retailshop.entity;
+
+public class Supplier {
+}
